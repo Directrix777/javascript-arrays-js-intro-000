0b9105fe8654e5b1b9862516004a0f2389a57564
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 18:47:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,13 +37,13 @@ function removeElementFromBeginningOfArray(array)
 {
   return array.slice(1)
 }
-/*
+
 function destructivelyRemoveElementFromEndOfArray(array)
 {
   array.pop()
   return array
 }
-
+/*
 function removeElementFromEndOfArray(array)
 {
   return array.slice(0, array.length - 1)
