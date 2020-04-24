cefed8b15d4b2c1e09d1556414747ac3a96b7f84
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 18:43:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,12 +10,12 @@ function addElementToBeginningOfArray(array, element)
 {
   return [element, ...array]
 }
-/*
+
 function addElementToEndOfArray(array, element)
 {
   return[...array, element]
 }
-
+/*
 function destructivelyAddElementToEndOfArray(array, element)
 {
   array.push(element)
