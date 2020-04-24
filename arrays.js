e83c70c8fb06d6faf39e7d495a920f901fba0d56
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 18:32:22 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  return array.unshift(element)
+}
+
+function addElementToBeginningOfArray(array, element)
+{
+  return [element ...array]
+}
+
+function addElementToEndOfArray(array, element)
+{
+  return[...array, element]
+}
+
+function destructivelyAddElementToEndOfArray
+{
+  return array.push(element)
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray
+{
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array)
+{
+  return array.slice(0, array.length - 1)
+}
