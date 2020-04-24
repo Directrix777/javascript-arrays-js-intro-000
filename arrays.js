1875e15ff616a74a2b492a9eaa1dc36054e4bc7f
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 18:42:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,6 @@ function destructivelyAddElementToBeginningOfArray(array, element)
   array.unshift(element)
   return array
 }
-
 function addElementToBeginningOfArray(array, element)
 {
   return [element, ...array]
