f00bf4308a76471f1190607cd9311fb3ca9481a9
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-24 18:34:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
-/*
+
 function destructivelyAddElementToBeginningOfArray(array, element)
 {
   return array.unshift(element)
 }
-
+/*
 function addElementToBeginningOfArray(array, element)
 {
   return [element ...array]
